b54584ba53f197fc4a211ecb844c5a1e61e3a421
dmswl6310 <dmswl6310@gmail.com>
2024-06-21 05:07:20 +0000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,7 +22,7 @@ export default withAuth({
           console.log(error);
         }
         // 배포버전 테스트, 해결 후 복구
-        // return false;
+        return false;
       }
 
       return true; // true 반환 시 로그인 허용
